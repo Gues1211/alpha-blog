321e01d9c3b711bbfa7a848fea38b38f385d867d
Guesly Germain <germain1211@outlook.com>
2017-12-27 15:05:17 +0000

    styled messages and errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
         @article.user = User.first
         
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
         else
             render :new
@@ -33,7 +33,7 @@ class ArticlesController < ApplicationController
     def update
         @article = Article.find(params[:id])
         if @article.update(article_params)
-            flash[:notice] = "Article successfully updated"
+            flash[:success] = "Article successfully updated"
             redirect_to article_path(@article)
         else
             render "edit"
@@ -48,7 +48,7 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         @article.destroy 
         
-        flash[:notice] = "Article was successfully deleted"
+        flash[:danger] = "Article was successfully deleted"
         
         redirect_to articles_path
     end 
